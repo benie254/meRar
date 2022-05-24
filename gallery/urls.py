583200ad47e408cea00b6ccd7f4dde09c3cdfe92
Benie <davinci.monalissa@gmail.com>
2022-05-24 07:08:55 +0300

    create a regex pattern for galleries from past days

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url('^today/$',views.gallery_today,name='galleryToday')
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name='pastGallery')
 ]
