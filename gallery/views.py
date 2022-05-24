6eadeca9389ffecf4fad92551b12436c3272b780
Benie <davinci.monalissa@gmail.com>
2022-05-24 07:04:30 +0300

    create function that returns the weekday

--- gallery/views.py
+++ gallery/views.py
@@ -14,3 +14,19 @@ def gallery_today(request):
     html = f"""
 """
     return HttpResponse(html)
+
+
+def convert_dates(dates):
+    """
+    :param dates: date of the week
+    :return: weekday for the date
+    """
+
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday']
+
+    # return the actual day of the week
+    day = days[day_number]
+
+    return day
