21771c83f16d1895556e5a423ee348ee6c6edfd5
Benie <davinci.monalissa@gmail.com>
2022-05-26 03:43:29 +0300

    update gallery urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,5 +1,7 @@
 from django.urls import re_path as url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -7,3 +9,6 @@ urlpatterns = [
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name='pastGallery'),
     url(r'^search/',views.search_results,name='search_results')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
