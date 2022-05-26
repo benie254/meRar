935a8e5574fadb1e6cd980b54807df54c38574f4
Benie <davinci.monalissa@gmail.com>
2022-05-26 03:57:02 +0300

    fix minor error in gallery/urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^$',views.gallery_today,name='galleryToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name='pastGallery'),
     url(r'^search/',views.search_results,name='search_results'),
-    url(r'^image/(\d+)',views.image,name=image)
+    url(r'^image/(\d+)',views.image,name='image')
 ]
 
 if settings.DEBUG:
