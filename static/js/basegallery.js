1dfb19e8597b946ac2fd3cb4ef8dbd4e55cef285
Benie <davinci.monalissa@gmail.com>
2022-05-27 04:49:04 +0300

    still testing new modal

--- static/js/basegallery.js
+++ static/js/basegallery.js
@@ -1,16 +1,20 @@
-var modal = document.getElementById("modal");
+// Get the modal
+var modal = document.getElementById("myModal");
 
-var pic = document.getElementById("pic");
-var modal_pic = document.getElementById("modal-pic");
-var modal_text = document.getElementById("modal-text");
-pic.onclick = function(){
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
   modal.style.display = "block";
-  modal_pic.src = this.src;
-  modal_text.innerHTML = this.alt;
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
 }
 
-var span = document.getElementsByClassName("exit")[0];
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
+// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
