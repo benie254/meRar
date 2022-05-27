2546092ff168c6d611b1dab77506d0e87d114011
Benie <davinci.monalissa@gmail.com>
2022-05-27 04:30:05 +0300

    update app

--- static/js/basegallery.js
+++ static/js/basegallery.js
@@ -1,15 +1,15 @@
 var modal = document.getElementById("modal");
 
-var img = document.getElementById("pic");
-var modalImg = document.getElementById("pic");
-var captionText = document.getElementById("modal-text");
-img.onclick = function(){
+var pic = document.getElementById("pic");
+var modal_pic = document.getElementById("modal-pic");
+var modal_text = document.getElementById("modal-text");
+pic.onclick = function(){
   modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+  modal_pic.src = this.src;
+  modal_text.innerHTML = this.alt;
 }
 
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("exit")[0];
 
 span.onclick = function() {
   modal.style.display = "none";
